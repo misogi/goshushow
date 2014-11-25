ed429df2fdfb40c0cd6afc8d5c470d68025f196d
Ryudo Nakagawa <nkgw.ry@gmail.com>
2014-11-25 18:30:00 +0900

    go fmt

--- hello.go
+++ hello.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-    "github.com/ChimeraCoder/anaconda"
+	"github.com/ChimeraCoder/anaconda"
 	"net/http"
 )
 
@@ -11,11 +11,11 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    anaconda.SetConsumerKey("-")
-    anaconda.SetConsumerSecret("your-consumer-secret")
-    api := anaconda.NewTwitterApi("your-access-token", "your-access-token-secret")
-    searchResult, _ := api.GetSearch("golang", nil)
-    for _ , tweet := range searchResult {
-        fmt.Println(tweet.Text)
-    }
+	anaconda.SetConsumerKey("-")
+	anaconda.SetConsumerSecret("your-consumer-secret")
+	api := anaconda.NewTwitterApi("your-access-token", "your-access-token-secret")
+	searchResult, _ := api.GetSearch("golang", nil)
+	for _, tweet := range searchResult {
+		fmt.Println(tweet.Text)
+	}
 }
