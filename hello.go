05d594d7bebd29e8b85ff4fbfacd32e1db497918
Ryudo Nakagawa <nkgw.ry@gmail.com>
2014-11-25 18:25:24 +0900

    add anaconda

--- hello.go
+++ hello.go
@@ -1,14 +1,21 @@
-package hello
+package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+    "github.com/ChimeraCoder/anaconda"
+	"net/http"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+    anaconda.SetConsumerKey("-")
+    anaconda.SetConsumerSecret("your-consumer-secret")
+    api := anaconda.NewTwitterApi("your-access-token", "your-access-token-secret")
+    searchResult, _ := api.GetSearch("golang", nil)
+    for _ , tweet := range searchResult {
+        fmt.Println(tweet.Text)
+    }
 }
